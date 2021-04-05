041f8d69222cb2b03731ca37d48f32af9ed0f4cb
George Huliaris <georgehuliaris@me.com>
2021-04-04 19:23:14 -0500

    tying employee and index js files

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const Employee = require("./employee.js")
 const inquirer = ("inquirer");
 
 const intern = (remainingInfo) => {
     inquirer.prompt({message: "school?", "name": "first"})
+    .then(({first, ...rest}))
+},
+const manager = (remainingInfo) => {
+    inquirer.prompt({message: "Office Number:", "name": "first"})
     .then(({first, ...rest}))
 }
