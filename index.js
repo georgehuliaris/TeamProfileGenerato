b8ef21a1dd29ad79ffb30eb7bd2233d77458b2d4
George Huliaris <georgehuliaris@me.com>
2021-04-05 17:11:48 -0500

    added links to other pages on index

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const Employee = require("./employee.js")
+const Employee = require("./employee");
+const Engineer = require("./engineer");
+const Manager = require("./manager.test");
+const Intern = require("./intern");
+
 const inquirer = ("inquirer");
+const fs = require("fs");
 
 const intern = (remainingInfo) => {
     inquirer.prompt({message: "school?", "name": "first"})
@@ -9,4 +14,3 @@ const manager = (remainingInfo) => {
     inquirer.prompt({message: "Office Number:", "name": "first"})
     .then(({first, ...rest}))
 }
-
